fed0859b94bdc6e95007abf547ba3f42e11ace18
mt0916264 <mateuspandinhalopes@hotmail.com>
2024-10-10 07:56:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll('a[href*="https://mt0916264.github.io/ND/Gerador/index.html"]');
-    links.forEach(link => {
-        link.style.display = 'none';
-        link.remove(); // Remove o link do DOM
-    });
-});
+
